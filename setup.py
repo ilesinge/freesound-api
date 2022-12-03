c879be777ff0ebeeba245f8148bcef880078cd61
Alexandre G.-Raymond <alex@ndre.gr>
2022-12-03 14:56:31 +0100

    Switch to poetry

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from distutils.core import setup
 
-setup(
-    name="freesound-api",
-    version="1.1",
-    py_modules=["freesound"],
-    install_requires=["requests<3.0,>2.27"],
-    python_requires=">=3.6",
-)
+if __name__ == "__main__":
+    setuptools.setup()
+
